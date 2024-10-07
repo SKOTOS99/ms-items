e600f2abdc4ec9a55114774ca5a5baf3c547f86b
SKOTOS99 <felipd95@hotmail.com>
2024-10-07 11:54:11 -0600

    initial commit

---
+++ src/main/java/com/items/app/MsItemsApplication.java
@@ -0,0 +1,13 @@
+package com.items.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsItemsApplication.class, args);
+	}
+
+}
