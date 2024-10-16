6f8c61ece07da9bd9cf12692491ad83dd141da3a
SKOTOS99 <felipd95@hotmail.com>
2024-10-16 10:55:01 -0600

    recive params and headerfrom api gateway

--- src/main/java/com/items/app/controllers/ItemController.java
+++ src/main/java/com/items/app/controllers/ItemController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.items.app.models.Item;
@@ -23,7 +25,11 @@ public class ItemController {
 	}
 	
 	@GetMapping("/item/listar")
-	public List<Item> listarItems(){
+	public List<Item> listarItems(@RequestParam(name="name", required=false) String name, 
+			@RequestHeader(name="token-request", required=false) String token){
+		System.out.println("token item: "+ token);
+		System.out.println("param item: "+ name);
+
 		return service.findAll();
 	}
 	
